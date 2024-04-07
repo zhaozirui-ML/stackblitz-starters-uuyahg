9f7c8ff6e104951037d1c4104791c90ef4fd47b8
zhaozirui-ML <zhaozirui721@gmail.com>
2024-04-07 20:55:36 +0800

    新增一个测试组件

--- app/hello/[myname]/page.tsx
+++ app/hello/[myname]/page.tsx
@@ -7,8 +7,18 @@ interface IProps{
 
 export default function Home(props:IProps) {
   return (
-    <main>
-     hello,myfriend,{props.params.myname}
+    <main style={
+      {
+        height:300
+      }
+    }>
+     <details className="dropdown">
+  <summary className="m-1 btn">open or close</summary>
+  <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</details>
     </main>
   );
 }
